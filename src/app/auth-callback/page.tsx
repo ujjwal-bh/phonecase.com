3cf57705e4d9d2757df9453879a45b6bdecde6b6
ujjwal-bh <ujjwalbh001@gmail.com>
2024-07-01 11:53:32 +0530

    payment

---
+++ src/app/auth-callback/page.tsx
@@ -0,0 +1,44 @@
+"use client";
+import { useQuery } from "@tanstack/react-query";
+import React, { useEffect, useState } from "react";
+import { getAuthStatus } from "./action";
+import { useRouter } from "next/navigation";
+import { Loader2 } from "lucide-react";
+
+const page = () => {
+    const router = useRouter()
+  const [configId, setConfigId] = useState<string | null>(null);
+  useEffect(() => {
+    let configurationId = localStorage.getItem("PHONECASE_CONFIGID");
+    if (configurationId) setConfigId(configurationId);
+  });
+
+
+  const {data} = useQuery({
+    queryKey: ["auth-callback"],
+    queryFn: async () => await getAuthStatus(),
+    retry: true,
+    retryDelay: 500
+  }) 
+
+  if(data?.success){
+    if(configId){
+        localStorage.removeItem("PHONECASE_CONFIGID")
+        router.push(`/configure/preview?id=${configId}`)
+    } else {
+        router.push("/")
+    }
+  }
+
+  return (
+    <div className="w-full mt-24 flex justify-center">
+        <div className="flex flex-col items-center gap-2">
+            <Loader2 className="h-8 w-8 animate-spin text-primary"/>
+            <h3 className="font-semibold text-xl">Logging you in...</h3>
+            <p>You will be redirected automatically.</p>
+        </div>
+    </div>
+  )
+};
+
+export default page;
--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -9,15 +9,25 @@ import { useMutation } from "@tanstack/react-query";
 import { ArrowRight, Check } from "lucide-react";
 import React, { useEffect, useState } from "react";
 import Confetti from "react-dom-confetti";
+import { createCheckoutSession } from "./actions";
+import { useRouter } from "next/navigation";
+import { useToast } from "@/components/ui/use-toast";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs"
+import LoginModal from "@/components/LoginModal";
 
 const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
+  const router = useRouter()
+  const {toast} = useToast()
+  const {user } = useKindeBrowserClient()
+
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false)
   const [showConfetti, setShowConfetti] = useState(false);
+
   useEffect(() => {
     setShowConfetti(true);
   }, []);
 
   const { color, model, finish, material } = configuration;
-
   const tw = COLORS.find(
     (supportedColor) => supportedColor.value === color
   )?.tw;
@@ -31,11 +41,36 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
   if(finish === "textured") totalPrice += PRODUCT_PRICES.finish.textured;
 
 
-  const {} = useMutation({
+  const {mutate: createPaymentSession} = useMutation({
     mutationKey: ["get-checkout-session"],
-    // mutationFn: null
+    mutationFn:  createCheckoutSession,
+    onSuccess: ({url}) => {
+      if(url){
+        router.push(url)
+      } else {
+        throw new Error("Unable to retireve payment URL")
+      }
+    },
+    onError: ()=> {
+      toast({
+        title: "something went wrong",
+        description: "there was an error on our end please try again.",
+        variant: "destructive"
+      })
+    }
   })
 
+
+  const handleCheckout = () => {
+    if(user){
+      // create payment session
+      createPaymentSession({configId: configuration.id})
+    } else {
+      // need to login
+      localStorage.setItem("PHONECASE_CONFIGID", configuration.id)
+      setIsLoginModalOpen(true)
+    }
+  }
   return (
     <>
       <div
@@ -47,6 +82,7 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
           config={{ elementCount: 200, spread: 90 }}
         />
       </div>
+      <LoginModal isOpen={isLoginModalOpen} setIsOpen={setIsLoginModalOpen}/>
       <div className="mt-20 grid grid-cols-1 text-sm sm:grid-cols-12 sm:grid-rows-1 sm:gap-x-6 md:gap-x-8 lg:gap-x-12">
         <div className="sm:col-span-4 md:col-span-3 md:row-span-2 md:row-end-2">
           <Phone
@@ -116,7 +152,7 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
               </div>
             </div>
             <div className="mt-8 flex justify-end pb-12">
-                <Button className="px-4 sm:px-6 lg:px-8">Check out <ArrowRight className="h-4 w-4 ml-1.5 inline"/></Button>
+                <Button className="px-4 sm:px-6 lg:px-8" onClick={handleCheckout}>Check out <ArrowRight className="h-4 w-4 ml-1.5 inline"/></Button>
 
             </div>
           </div>
