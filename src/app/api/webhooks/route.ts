fe126d0cb671c63ab1fc31c3fcbf34524d51fe67
ujjwal-bh <ujjwalbh001@gmail.com>
2024-07-01 14:24:44 +0530

    email on completion configuration

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -1,43 +1,47 @@
-import { db } from '@/db'
-import { stripe } from '@/lib/stripe'
-import { headers } from 'next/headers'
-import { NextResponse } from 'next/server'
-import Stripe from 'stripe'
+import { db } from "@/db";
+import { stripe } from "@/lib/stripe";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+import { Resend } from "resend";
+import OrderReceivedEmail from "@/components/email/OrderReceivedEmail";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: Request) {
   try {
-    const body = await req.text()
-    const signature = headers().get('stripe-signature')
+    const body = await req.text();
+    const signature = headers().get("stripe-signature");
 
     if (!signature) {
-      return new Response('Invalid signature', { status: 400 })
+      return new Response("Invalid signature", { status: 400 });
     }
 
     const event = stripe.webhooks.constructEvent(
       body,
       signature,
       process.env.STRIPE_WEBHOOK_SECRET!
-    )
+    );
 
-    if (event.type === 'checkout.session.completed') {
+    if (event.type === "checkout.session.completed") {
       if (!event.data.object.customer_details?.email) {
-        throw new Error('Missing user email')
+        throw new Error("Missing user email");
       }
 
-      const session = event.data.object as Stripe.Checkout.Session
+      const session = event.data.object as Stripe.Checkout.Session;
 
       const { userId, orderId } = session.metadata || {
         userId: null,
         orderId: null,
-      }
+      };
 
       if (!userId || !orderId) {
-        throw new Error('Invalid request metadata')
+        throw new Error("Invalid request metadata");
       }
 
-      const billingAddress = session.customer_details!.address
-    //   @ts-ignore
-      const shippingAddress = session.shipping!.address
+      const billingAddress = session.customer_details!.address;
+      //   @ts-ignore
+      const shippingAddress = session.shipping!.address;
 
       const updatedOrder = await db.order.update({
         where: {
@@ -66,16 +70,34 @@ export async function POST(req: Request) {
             },
           },
         },
-      })
+      });
+      await resend.emails.send({
+        from: "phonecase.com <ujjwalbh001@gmail.com>",
+        to: [event.data.object.customer_details.email],
+        subject: "Thanks for your order",
+        react: OrderReceivedEmail({
+          orderId,
+          orderDate: updatedOrder.createdAt.toLocaleDateString(),
+        //  @ts-ignore
+          shippingAddress: {
+            name: session.customer_details!.name!,
+            city: shippingAddress!.city!,
+            country: shippingAddress!.country!,
+            postalCode: shippingAddress!.postal_code!,
+            street: shippingAddress!.line1!,
+            state: shippingAddress!.state,
+          },
+        }),
+      });
     }
 
-    return NextResponse.json({ result: event, ok: true })
+    return NextResponse.json({ result: event, ok: true });
   } catch (err) {
-    console.error(err)
+    console.error(err);
 
     return NextResponse.json(
-      { message: 'Something went wrong', ok: false },
+      { message: "Something went wrong", ok: false },
       { status: 500 }
-    )
+    );
   }
 }
